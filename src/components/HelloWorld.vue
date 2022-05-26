a0f9069853274c1d25a1dd1aff1660990a884709
Victor Hernandez <victorm96hernandez@gmail.com>
2022-05-25 23:24:18 -0600

    'v2'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,46 +3,76 @@
     <Content>
       <a-row>
         <!--========Guardar========-->
-        <a-button @click="showModal" class="editable-add-btn">
+        <a-button @click="add = true" class="editable-add-btn">
           Guardar
         </a-button>
 
         <!--========Table========-->
         <a-col :span="24">
           <a-table :columns="columns" :dataSource="dataSource" :loading="loading" rowKey="id" bordered>
-            <!--=Estado=-->
+            <!--Estado-->
             <template #estado="{ record }">
               <a-tag color="#87d068" v-if="record.estado == 'Activado'">{{
                   record.estado
               }}</a-tag>
               <a-tag color="#f50" v-else>{{ record.estado }}</a-tag>
             </template>
-            <!--=Eliminar=-->
+            <!--Acciones-->
             <template #acciones="{ record }">
-              <a-button :loading="record.loading" @click="eliminar(record.id), (record.loading = true)">Eliminar
-              </a-button>
+              <a-row>
+                <a-col :span="12">
+                  <a-button @click="destroy(record.id)">
+                    <DeleteOutlined />
+                  </a-button>
+                </a-col>
+                <a-col :span="12">
+                  <a-button @click="patch(record.id)">
+                    <EditOutlined />
+                  </a-button>
+                </a-col>
+              </a-row>
             </template>
           </a-table>
         </a-col>
       </a-row>
 
-      <!--========Modal========-->
-      <a-modal v-model:visible="visible" title="Guardar">
-        <!--=Formulario=-->
-        <a-form id="Form" layout="vertical" autocomplete="off" :model="formState" @finish="guardar">
+      <!--========Add========-->
+      <a-modal v-model:visible="add" title="Guardar">
+        <!--Formulario-->
+        <a-form id="Form" layout="vertical" autocomplete="off" :model="formState" @finish="post">
           <a-form-item label="Name" name="name" :rules="[{ required: true, message: 'Requerido' }]">
-            <a-input v-model:value="formState.name" />
+            <a-input type="text" v-model:value="formState.name" placeholder="..." />
           </a-form-item>
           <a-form-item label="Email" name="email" :rules="[{ required: true, message: 'Requerido' }]">
-            <a-input v-model:value="formState.email" />
+            <a-input type="email" v-model:value="formState.email" placeholder="..." />
           </a-form-item>
           <a-form-item label="Estado" name="estado" :rules="[{ required: true, message: 'Requerido' }]">
-            <a-input v-model:value="formState.estado" />
+            <a-textarea v-model:value="formState.estado" placeholder="..." :rows="4" showCount :maxlength="100" />
           </a-form-item>
         </a-form>
-        <!--=Footer=-->
+        <!--Footer-->
         <template #footer>
-          <a-button type="primary" form="Form" key="submit" htmlType="submit">Enviar</a-button>
+          <a-button type="primary" form="Form" key="submit" htmlType="submit">Submit</a-button>
+        </template>
+      </a-modal>
+
+      <!--========Update========-->
+      <a-modal v-model:visible="update" title="Editar">
+        <!--Formulario-->
+        <a-form id="Form" layout="vertical" autocomplete="off" :model="formState" @finish="put">
+          <a-form-item label="Name" name="name" :rules="[{ required: true, message: 'Requerido' }]">
+            <a-input type="text" v-model:value="formState.name" placeholder="..." />
+          </a-form-item>
+          <a-form-item label="Email" name="email" :rules="[{ required: true, message: 'Requerido' }]">
+            <a-input type="email" v-model:value="formState.email" placeholder="..." />
+          </a-form-item>
+          <a-form-item label="Estado" name="estado" :rules="[{ required: true, message: 'Requerido' }]">
+            <a-textarea v-model:value="formState.estado" placeholder="..." :rows="4" showCount :maxlength="100" />
+          </a-form-item>
+        </a-form>
+        <!--Footer-->
+        <template #footer>
+          <a-button type="primary" form="Form" key="submit" htmlType="submit" danger>Submit</a-button>
         </template>
       </a-modal>
     </Content>
@@ -56,9 +86,11 @@ import { ref } from "vue";
 import { reactive } from "vue";
 import { Form } from "ant-design-vue";
 import { message } from "ant-design-vue";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons-vue";
 
 const useForm = Form.useForm;
 
+//Table
 const columns = [
   {
     title: "Id",
@@ -89,7 +121,7 @@ const columns = [
     title: "Acciónes",
     dataIndex: "acciones",
     slots: { customRender: "acciones" },
-  },
+  }
 ];
 
 export default {
@@ -97,41 +129,40 @@ export default {
     return {
       //Get
       dataSource: null,
-      columns,
+      columns
     };
   },
 
   setup() {
     //Modal
-    const visible = ref(false);
+    const add = ref(false);
+    const update = ref(false);
 
-    const showModal = () => {
-      visible.value = true;
-    };
-    const notModal = () => {
-      visible.value = false;
-      resetFields();
-    };
-
-    //Post
+    //Formulario
     const formState = reactive({
+      id: null,
       name: null,
       email: null,
-      estado: null,
+      estado: null
     });
 
     const { resetFields } = useForm(formState, reactive({}));
 
     return {
-      //modal
-      visible,
-      showModal,
-      notModal,
+      //Modal
+      add,
+      update,
 
       //Post
       formState,
       resetFields,
     };
+  },
+
+  components: {
+    //Icons
+    DeleteOutlined,
+    EditOutlined,
   },
 
   mounted() {
@@ -142,35 +173,60 @@ export default {
     //Get
     get() {
       axios
-        .get("http://api.test/public/api/user")
+        .get("http://api.test/api/user")
         .then((response) => {
-          console.log(response)
           this.dataSource = response.data;
         })
         .catch((e) => console.log(e));
     },
 
     //post
-    guardar(values) {
+    post(values) {
       axios
-        .post("http://api.test/public/api/user", values)
+        .post("http://api.test/api/user", values)
         .then((response) => {
           this.get();
-          this.notModal();
+          this.resetFields();
+          this.add = false;
           message.success("Guardado");
-          return response;
         })
         .catch((e) => console.log(e));
     },
 
-    //Delete
-    eliminar(id) {
+    //Patch
+    patch(id) {
       axios
-        .delete("http://api.test/public/api/user/" + id)
+        .get("http://api.test/api/user/" + id)
+        .then((response) => {
+          this.formState.id = response.data.id;
+          this.formState.name = response.data.name;
+          this.formState.email = response.data.email;
+          this.formState.estado = response.data.estado;
+          this.update = true;
+        })
+        .catch((e) => console.log(e));
+    },
+
+    //Put
+    put(values) {
+      axios
+        .put("http://api.test/api/user/" + this.formState.id, values)
         .then((response) => {
           this.get();
-          message.success("Eliminado");
-          return response;
+          this.resetFields();
+          this.update = false;
+          message.warning("Editado");
+        })
+        .catch((e) => console.log(e));
+    },
+
+    //Destroy
+    destroy(id) {
+      axios
+        .delete("http://api.test/api/user/" + id)
+        .then((response) => {
+          this.get();
+          message.error("Eliminado");
         })
         .catch((e) => console.log(e));
     },
